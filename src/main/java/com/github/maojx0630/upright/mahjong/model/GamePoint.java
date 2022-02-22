949c774e8f790e3a31d724b89314a04adb34b4e2
毛家兴 <908156949@qq.com>
2022-02-22 16:08:07 +0800

    init

--- src/main/java/com/github/maojx0630/upright/mahjong/model/GamePoint.java
+++ src/main/java/com/github/maojx0630/upright/mahjong/model/GamePoint.java
@@ -4,34 +4,40 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
-/**
- * 对局点数
- **/
+/** 对局点数 */
 @Data
 @TableName("game_point")
 public class GamePoint {
 
- /**
-  * 对局id
-  **/
+  /** 对局id */
   @TableField("game_id")
   private Long gameId;
 
-  /**
-   * 选手id
-   **/
+  /** 选手id */
   @TableField("user_id")
   private Long userId;
 
-  /**
-   * 选手点数
-   **/
+  /** 选手点数 */
   @TableField("points")
   private Integer points;
 
-  /**
-   * 选手顺位
-   **/
+  /** 选手顺位 */
   @TableField("seq_order")
   private Integer seqOrder;
+
+  /** 战后R值 */
+  @TableField("after_rate")
+  private Double afterRate;
+
+  /** 战后分数 */
+  @TableField("after_score")
+  private Double afterScore;
+
+  /** R值变化 */
+  @TableField("change_rate")
+  private Double changeRate;
+
+  /** 分数变化 */
+  @TableField("change_score")
+  private Double changeScore;
 }
